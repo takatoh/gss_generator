9f418c64fb3d3b7d31cdd3ca869d6707918e154e
takatoh <takatoh.m@gmail.com>
2017-03-21 15:15:51 +0900

    lib/gss/polar_point.rb(GSS::PolarPoint#to_cartesian): New.

--- lib/gss/polar_point.rb
+++ lib/gss/polar_point.rb
@@ -11,6 +11,13 @@ module GSS
       @theta = theta
       @phi = phi
     end
+
+    def to_cartesian
+      x = @r * Math.sin(@theta) * Math.cos(@phi)
+      y = @r * Math.sin(@theta) * Math.sin(@phi)
+      z = @r * Math.cos(@theta)
+      [x, y, z]
+    end
   end   # of class PolarPoint
 
 end   # of module GSS
