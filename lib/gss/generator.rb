d74aa853e70ef4f636ad1b4af0f64f1870e623a4
takatoh <takatoh.m@gmail.com>
2017-03-22 12:57:04 +0900

    Adjust change of GSS::PolarPoint.

--- lib/gss/generator.rb
+++ lib/gss/generator.rb
@@ -6,18 +6,18 @@ require "gss/polar_point"
 module GSS
 
   class Generator
-    def generate(r, n, relocation = false)
+    def generate(n, relocation = false)
       theta_1 = Math::PI
       phi_1 = 0.0
 
       points = []
-      points << PolarPoint.new(r, theta_1, phi_1)
+      points << PolarPoint.new(theta_1, phi_1)
       2.upto(n) do |k|
         h_k = -1.0 + 2.0 * (k - 1) / (n - 1)
         theta_k = Math.acos(h_k)
         phi_k = points.last.phi + 3.6 / Math.sqrt(n) * 1 / Math.sqrt(1 - h_k ** 2)
         phi_k = phi_k.infinite? ? 0.0 : phi_k % (Math::PI * 2.0)
-        points << PolarPoint.new(r, theta_k, phi_k) 
+        points << PolarPoint.new(theta_k, phi_k) 
       end
 
       if relocation
