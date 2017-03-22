5a4a0fe6394acdc00c86ee76e7fb24743661a92c
takatoh <takatoh.m@gmail.com>
2017-03-22 12:39:52 +0900

    lib/gss/generator.rb: Feature relocation first and last point.

--- lib/gss/generator.rb
+++ lib/gss/generator.rb
@@ -6,7 +6,7 @@ require "gss/polar_point"
 module GSS
 
   class Generator
-    def generate(r, n)
+    def generate(r, n, relocation = false)
       theta_1 = Math::PI
       phi_1 = 0.0
 
@@ -20,8 +20,22 @@ module GSS
         points << PolarPoint.new(r, theta_k, phi_k) 
       end
 
+      if relocation
+        points[0] = relocate(points[1], points[2], points[4], points[5], points[6])
+        points[-1] = relocate(points[-2], points[-3], points[-5], points[-6], points[-7])
+      end
+
       points
     end
+
+    private
+
+      def relocate(p1, p2, p3, p4, p5)
+        c = [p1, p2, p3, p4, p5].map{|p| p.to_cartesian }
+        c = c.transpose.map{|l| l.inject(&:+) / 5.0 }
+        PolarPoint.from_cartesian(c[0], c[1], c[2])
+      end
+
   end   # of class Generator
 
 end   # of module GSS
