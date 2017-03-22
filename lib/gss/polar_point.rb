d9e6a62c3f9832c80e8fea9ea38bbe4131bc1528
takatoh <takatoh.m@gmail.com>
2017-03-22 12:45:40 +0900

    lib/gss/polar_point.rb(GSS::PolarPoint): Restrict target to unit sphere.

--- lib/gss/polar_point.rb
+++ lib/gss/polar_point.rb
@@ -4,26 +4,24 @@
 module GSS
 
   class PolarPoint
-    attr_reader :r, :theta, :phi
+    attr_reader :theta, :phi
 
-    def initialize(r, theta, phi)
-      @r = r
+    def initialize(theta, phi)
       @theta = theta
       @phi = phi
     end
 
     def to_cartesian
-      x = @r * Math.sin(@theta) * Math.cos(@phi)
-      y = @r * Math.sin(@theta) * Math.sin(@phi)
-      z = @r * Math.cos(@theta)
+      x = Math.sin(@theta) * Math.cos(@phi)
+      y = Math.sin(@theta) * Math.sin(@phi)
+      z = Math.cos(@theta)
       [x, y, z]
     end
 
     def self.from_cartesian(x, y, z)
-      r = Math.sqrt(x * x + y * y + z * z)
-      theta = Math.acos(z / r)
+      theta = Math.acos(z / (x * x + y * y + z * z))
       phi = Math.atan2(y, x)
-      self.new(r, theta, phi)
+      self.new(theta, phi)
     end
   end   # of class PolarPoint
 
